d2df11bd521bfd04315b67cc566fabe5d5549bc1
Admin <nghi08122003@gmail.com>
2024-06-26 13:14:44 +0700

    add open accuracy

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import numpy as np
 import ruamel_yaml as yaml
 import json
 
@@ -8,30 +9,21 @@ warnings.filterwarnings('ignore')
 
 def main(args):
     total = 0
-    number_of_yn_ques = 0
-    yn_right = 0
-    if (args.inference == 'rank'):
-        output_data = None
-        with open(f'{args.output_dir}/result/vqa_result.json') as fi:
-            output_data = json.load(fi)
-        for item in output_data:
-            total += 1
-            if (item['ground_truth'] in ['yes', 'no']):
-                number_of_yn_ques += 1
-                yn_right += int(item['ground_truth'] == item['model_answer'])
-        with open(f'{args.output_dir}/result/evaluation_result.json', 'w') as fo:
-            json.dump({'total' : total, 'yn_ques' : number_of_yn_ques, 'yn_right' : yn_right, 'yn_right_rate' : yn_right/number_of_yn_ques}, fo)
-    else:
-        output_data = None
-        with open(f'{args.output_dir}/result/vqa_result.json') as fi:
-            output_data = json.load(fi)
-        for item in output_data:
-            total += 1
-            if (item['ground_truth'] in ['yes', 'no']):
-                number_of_yn_ques += 1
-                yn_right += int(item['ground_truth'] == item['model_answer'])
-        with open(f'{args.output_dir}/result/evaluation_result.json', 'w') as fo:
-            json.dump({'total' : total, 'yn_ques' : number_of_yn_ques, 'yn_right' : yn_right, 'yn_right_rate' : yn_right/number_of_yn_ques}, fo)
+    number_of_closed_ques = 0
+    open_right = 0
+    closed_right = 0
+    output_data = None
+    with open(f'{args.output_dir}/result/vqa_result.json') as fi:
+        output_data = json.load(fi)
+    for item in output_data:
+        total += 1
+        if (item['ground_truth'] in ['yes', 'no']):
+            number_of_closed_ques += 1
+            closed_right += int(item['ground_truth'] == item['model_answer'])
+        else:
+            open_right += int(item['ground_truth'] == item['model_answer'])
+    with open(f'{args.output_dir}/result/evaluation_result.json', 'w') as fo:
+        json.dump({'total' : total, 'closed_ques' : number_of_closed_ques, 'closed_right' : closed_right, 'open_right' : open_right, 'closed_accuracy' : np.round((closed_right/number_of_closed_ques)*100.00, 6), 'open_accuracy': np.round((open_right/(total - number_of_closed_ques))*100.0, 6), 'total_accuracy':np.round((open_right + closed_right)/total*100.0, 6)}, fo)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
